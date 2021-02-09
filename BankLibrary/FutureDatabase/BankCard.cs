622169d23131e196e015f791b02b18c5811353ff
Andrey1771 <Hohlov908@gmail.com>
2021-02-09 22:34:13 +0300

    Implement BankCard logic

--- BankLibrary/FutureDatabase/BankCard.cs
+++ BankLibrary/FutureDatabase/BankCard.cs
@@ -5,26 +5,73 @@ using BankLibrary.Client;
 
 namespace BankLibrary.FutureDatabase
 {
-    class BankCard
+    class BankCard 
     {
+        static SortedSet<ulong> busyIdSortedSet;
+
         ulong id;
-        public ulong ID { get { return id; } private set { id = value; } }
-        string name;
-        public string CardholderName { get { return name; } }
+        public ulong ID { get { return id; }
+            set
+            {
+                if (!TryAddId(value))
+                    throw new Exception("Все плохо, newVal == uint.MaxValue");
+                id = value;
+            }
+        }
+
+        public string CardholderName { get; }
         public DateTime ValidDate { get; }
         public int SecretNumber { get; }
 
-        public BankCard(string aName, uint bim, uint individualNumber, int aSecretNumber, DateTime aValidDate = default(DateTime))
+
+        public BankCard(string aCardholderName, uint abim, uint individualNumber, int aSecretNumber, DateTime aValidDate = default(DateTime))
         {
-            name = aName;
-            ID = bim * (ulong)Math.Pow(10, 6) + individualNumber;
+            CardholderName = aCardholderName;
+            ID = abim * (ulong)Math.Pow(10, 6) + individualNumber;
             SecretNumber = aSecretNumber;
             ValidDate = aValidDate.Date;
             if (aValidDate == default(DateTime))
             {
                 ValidDate = DateTime.Now.Date;
             }
+            
+        }
 
+        public bool TryAddId(ulong id)
+        {
+            if (!busyIdSortedSet.Add(id))//SortedSet.Add - O(log(k))
+            {
+                var newId = ulong.MinValue;
+                while (!busyIdSortedSet.Add(newId))//В худшем O(n * log(n)) 
+                {
+                    if (newId == ulong.MaxValue)
+                    {
+                        return false;
+                    }
+                    ++newId;
+                }
+            }
+            return true;
+        }
+
+        public static uint GetUniqueIndividualNumber(uint bim)
+        {
+            var newIndividualNumber = uint.MinValue;
+            while (!busyIdSortedSet.Add(newIndividualNumber))//В худшем O(n * log(n))
+            {
+                if (newIndividualNumber == 100_000_000)
+                {
+                    throw new Exception("Все ключи заняты, newIndividualNumber == 100_000_000");//Добавить тесты
+                }
+                ++newIndividualNumber;
+            }
+
+            return newIndividualNumber;
+        }
+
+        ~BankCard()
+        {
+            busyIdSortedSet.Remove(ID); ;
         }
     }
 }
--- BankLibrary/FutureDatabase/BankDeposit.cs
+++ BankLibrary/FutureDatabase/BankDeposit.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BankLibrary.FutureDatabase
 {
-    struct BankDeposit
+    class BankDeposit
     {
     }
 }
